64ef82463851edb62fc2afbf0a128c342b030ee5
yasin ansari <my5559647@gmail.com>
2026-09-10 19:27:30 +0500

    Remove compiled JS files from git and update .gitignore

--- backend/src/routes/leadRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const leadController_1 = require("../controllers/leadController");
-const router = (0, express_1.Router)();
-router.post('/start-scraping', leadController_1.startScrapingTask);
-exports.default = router;
